328a330f0b4f617aa4fcfb4b3fe4e10243549acf
DC <dillonchaffey@gmail.com>
2020-04-21 06:55:59 -0300

    1

--- static/js/main.js
+++ static/js/main.js
@@ -36,16 +36,16 @@ $(document).ready(function() {
     
     /* ======= Chart ========= */
     
-    $('.chart').easyPieChart({		
-		barColor:'#00BCD4',//Pie chart colour
-		trackColor: '#e8e8e8',
-		scaleColor: false,
-		lineWidth : 5,
-		animate: 2000,
-		onStep: function(from, to, percent) {
-			$(this.el).find('span').text(Math.round(percent));
-		}
-	});  
+  //   $('.chart').easyPieChart({		
+	// 	barColor:'#00BCD4',//Pie chart colour
+	// 	trackColor: '#e8e8e8',
+	// 	scaleColor: false,
+	// 	lineWidth : 5,
+	// 	animate: 2000,
+	// 	onStep: function(from, to, percent) {
+	// 		$(this.el).find('span').text(Math.round(percent));
+	// 	}
+	// });  
 	
 
     
